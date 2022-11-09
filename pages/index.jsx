816bdc96111d71870c57c7987fd6e9e2c15a6a77
aossucci <antonio.ossucci@gmail.com>
2022-11-09 16:30:32 -0300

    Exemplo com scss

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,11 @@
 import Title from '../src/components/title/title'
+import Button from '../src/components/button/button'
 
 export default function HomePage() {
-  return <Title>Página Inicial</Title>
+  return (
+    <>
+      <Title>Página Inicial</Title>
+      <Button>Botão</Button>
+    </>
+  )
 }
